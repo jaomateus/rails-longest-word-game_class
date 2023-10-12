d951b10c2448d05b7f6d774e28e1f15edcb82911
Joao Mateus <jaomateus@gmail.com>
2023-10-12 10:40:20 +0100

    finish project

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class GamesController < ApplicationController
 
   VOWELS = %w(A E I O U Y)
@@ -9,5 +11,20 @@ class GamesController < ApplicationController
   end
 
   def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
